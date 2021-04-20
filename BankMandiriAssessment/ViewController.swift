778c5ceb18348e28761a3bc5ee5c16f7515b54ac
Jacob Andrean <andreanjacob99@gmail.com>
2021-04-20 19:15:11 +0700

    Initial Commit

---
+++ BankMandiriAssessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankMandiriAssessment
+//
+//  Created by Jacob Andrean on 20/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
